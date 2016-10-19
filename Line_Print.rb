1a8bf0560b4b99bc8839b94eebee3c4c521fef15
ryanstan95 <ryanstan95@tamu.edu>
2016-10-19 13:22:44 -0500

    Print Lines
    Ensures that the file can be read

---
+++ Line_Print.rb
@@ -0,0 +1,5 @@
+apache_logs = File.readlines "This_is_a_test.txt"
+print apache_logs
+apache_logs(/\A(?<ip_address>\S+) \S+ \S+ \[(?<time>[^\]]+)\] "(?<method>GET|POST) (?<url>\S+) \S+?" (?<status>\d+) (?<bytes>\S+)/)
+print ip_address
+print bytes
