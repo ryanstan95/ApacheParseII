099309c90a94fe4420715da8f6088f5cb83a99b7
ryanstan95 <ryanstan95@tamu.edu>
2016-10-20 10:51:53 -0500

    Added date breaks
    thanks adam

--- rev5github.rb
+++ rev5github.rb
@@ -1,8 +1,22 @@
+require 'date'
+
 File.open("This_is_a_test.txt", "r") do |f|
 line_counter = 0
 url_array = []
+failarray = []
+redirectarray = []
+successarray = []
+errors_array = []
   f.each_line do |line|
     matches = (/\A(?<ip_address>\S+) \S+ \S+ \[(?<time>[^\]]+)\] "(?<method>GET|POST) (?<url>\S+) \S+?" (?<status>\d+) (?<bytes>\S+)/).match(line)
+    #checks for bustted-ass lines and dumps them into an array of errors
+    if !matches then
+	    errors_array.push(line)
+	    next
+    end
+    whole_day = Date.strptime(matches[:time], '%d/%b/%Y:%H:%M:%S')
+    last2 = whole_day.strftime('%Y-%m')
+    puts last2
     ip = matches[:ip_address]
     url = matches[:url]
     status = matches[:status]
@@ -11,10 +25,11 @@ url_array = []
     #add to the line counter
     line_counter = line_counter+1
     time = matches[:time]
-    puts time
+    puts status[0]
   end
   #the parsing of the file is finished at this point
   puts "DONE"
-  puts url_array
   print "There were", " ", line_counter,  " " ,"requests"
+  puts failarray
 end
+#test to revert comments
